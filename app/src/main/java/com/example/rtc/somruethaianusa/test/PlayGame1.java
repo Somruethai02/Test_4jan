0887bd9a37c6a290a61b1a357f6473ec9f0dec82
Somruthai <Somruthai_muchan@hotmail.com>
2017-01-16 15:25:48 +0700

    image ok

--- app/src/main/java/com/example/rtc/somruethaianusa/test/PlayGame1.java
+++ app/src/main/java/com/example/rtc/somruethaianusa/test/PlayGame1.java
@@ -9,5 +9,6 @@ public class PlayGame1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game1);
+
     }
 }
