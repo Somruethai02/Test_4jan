6d54b19fae992e7f288983ee2deeeabf90c681a4
masterUNG <phrombutr@gmail.com>
2017-01-16 08:52:38 +0700

    Step1

--- app/src/main/java/com/example/rtc/somruethaianusa/test/KnowleageActivity.java
+++ app/src/main/java/com/example/rtc/somruethaianusa/test/KnowleageActivity.java
@@ -1,7 +1,9 @@
 package com.example.rtc.somruethaianusa.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class KnowleageActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class KnowleageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_knowleage);
+    }   // Main Method
+
+    public void clickAnimal1(View view) {
+        startActivity(new Intent(KnowleageActivity.this, Animal1.class));
     }
-}
+
+    public void clickAnimal2(View view) {
+        startActivity(new Intent(KnowleageActivity.this, Animal2.class));
+    }
+
+}   // Main Class
