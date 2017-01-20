40a90d8cb9faa57af1ccf32823c99a4cb1065386
Somruthai <Somruthai_muchan@hotmail.com>
2017-01-20 12:50:32 +0700

    game over

---
+++ app/src/main/java/com/example/rtc/somruethaianusa/test/Gameover.java
@@ -0,0 +1,13 @@
+package com.example.rtc.somruethaianusa.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Gameover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gameover);
+    }
+}
